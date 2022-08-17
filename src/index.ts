8412ce940d095bafeb6c80e64afccdbe2bf47fbc
Paxton Edgar <paxtonedgar3@gmail.com>
2022-08-17 11:05:20 -0400

    init push

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-// your code here
+const inputString: string = (<HTMLInputElement>document.getElementById("inputString")).value;
+const numSpaces:number = parseInt((<HTMLInputElement>document.getElementById("numSpaces")).value);
+const button = (<HTMLButtonElement>document.getElementById("pad_it"));
+
+button!.addEventListener("click", function(){
+    console.log(prepend(inputString, numSpaces));
+});
+
+function prepend(inputString: string, numSpaces: number | string){
+
+  if(numSpaces!= null && inputString!= null){
+  let spaces: string ="";
+    for(let x=0; x<numSpaces;x++){
+    spaces = spaces + "&nbsp;";
+    }
+     let result = spaces + inputString;
+     let output = "\"" + result + "\"";
+     return output;
+
+  }
+  else return "";
+
+}
+
+console.log(prepend("happy", 10));
